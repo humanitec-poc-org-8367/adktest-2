4ec52c79bbbac2cdecf0719a9e93467795fb3c69
phil wheeler <prw.wheeler@gmail.com>
2023-06-09 09:56:06 +0000

    Calls tods

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ const http = require('http');
 const requestHandler = async (request, response) => {
   console.log(request.url);
 
+  const url = process.env.TODS_URL;
+  console.log(`GET time of day: url=${url}`);
+  const r = await fetch(url);
+  const body = await(r.json);
+  const tod = body.timeOfDay
+  console.log(`GET time of day: tod=${tod}`);
+  
   const html = `
   <html>
     <body>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
       <div class="container text-center mt-5 pt-5">
-        <h1>TODS_URL=${process.env.TODS_URL}</h1>
+        <p>TODS_URL: ${url}</p>
+        <p>Time of day: ${tod}</p>
       </div>
     </body>
   </html>
