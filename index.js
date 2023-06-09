272ba85e5df4e34f0a317cadb07474cd1ae00427
phil wheeler <prw.wheeler@gmail.com>
2023-06-09 09:42:25 +0000

    TODS

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
 const http = require('http');
-// const mysql = require('mysql2');
-
-// create the connection to database
-// const connection = mysql.createConnection({
-//   host: process.env.DB_HOST || 'localhost'  ,
-//   user: process.env.DB_USER || 'root',
-//   password: process.env.DB_PASSWORD || 'secret',
-//   database: process.env.DB_DATABASE || 'score',
-//   port: process.env.DB_PORT || 3306
-// });
 
 const requestHandler = async (request, response) => {
   console.log(request.url);
-
-  // Run hello world query
-  // const [rows, fields] = await connection.promise().query('SELECT "This is an example application deployed with Score!" as message');
-
-  // const message = rows[0].message;
 
   const html = `
   <html>
@@ -24,7 +9,7 @@ const requestHandler = async (request, response) => {
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
       <div class="container text-center mt-5 pt-5">
-        <h1>Hello World!</h1>
+        <h1>TODS_URL=${process.env.TODS_URL}</h1>
       </div>
     </body>
   </html>
